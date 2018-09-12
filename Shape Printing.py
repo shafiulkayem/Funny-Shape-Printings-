5fc4822b6e17083f55f971abf4f6fdf4689d82a0
Shafiul Kayem <37289068+shafiulkayem@users.noreply.github.com>
2018-09-12 22:30:37 +0600

    My Nickname

---
+++ Shape Printing.py
@@ -0,0 +1,8 @@
+# My Nickname! 
+
+print('      /     /\\  \\     /  |---- |\\  /|')
+print('     /  /  /__\\  \\___/   |___  | \\/ |') 
+print('    /-/   /    \\   |     |     |    |')
+print('   /   \\ /      \\  |     |____ |    |\n') 
+
+print('My Name is Shafiul Kayem! \n (Kayem) <-- is My Nickname.\n')
